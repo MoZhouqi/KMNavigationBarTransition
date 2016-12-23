feeb69c24bc251fe2307d52a75ade77673ef9eb2
huangboju <529940945@qq.com>
2016-12-23 15:35:20 +0800

    support swift3

--- Example/UIImage+Color.swift
+++ Example/UIImage+Color.swift
@@ -10,14 +10,14 @@ import UIKit
 
 public extension UIImage {
     
-    convenience init(color: UIColor, size: CGSize = CGSizeMake(1, 1)) {
+    convenience init(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
         color.setFill()
         UIRectFill(rect)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        self.init(CGImage: image.CGImage!)
+        self.init(cgImage: (image?.cgImage!)!)
     }
     
 }
